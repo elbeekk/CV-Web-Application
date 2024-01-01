91e1ace9b443855c8185cbd5053cfe97bf2cc655
elbek <elbekmirzamakhmudov@gmail.com>
2024-01-02 00:42:48 +0500

    many things done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:elbek/utils/functions.dart';
 import 'package:flutter/material.dart';
 import 'package:elbek/pages/main_page.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   AppFunction.init();
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -13,11 +15,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setApplicationSwitcherDescription(ApplicationSwitcherDescription(
+      label: "Elbek's CV1",
+      primaryColor: Theme.of(context).primaryColor.value, // This line is required
+    ));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: false),
       title: 'Elbek CV',
-      home: const Home(),
+      home: Title(color: Colors.white,
+      title: "Elbek's CV2",
+      child: const Home()),
     );
   }
 }
